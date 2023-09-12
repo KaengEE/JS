761a4e599110360866c2a80d56f093ba7429e694
KH <yopuiqwer@naver.com>
2023-09-12 14:51:33 +0900

    검색필터추가

--- 투두리스트/app.js
+++ 투두리스트/app.js
@@ -1,7 +1,8 @@
 //alert("test");
 
-const addForm = document.querySelector(".add"); //할일 추가 폼
+const addForm = document.querySelector("form.add"); //할일 추가 폼
 const list = document.querySelector(".todos"); //ul 리스트
+const search = document.querySelector("form.search"); //serch 폼
 
 addForm.addEventListener("submit", function (e) {
   e.preventDefault(); //이벤트중지
@@ -10,6 +11,7 @@ addForm.addEventListener("submit", function (e) {
   addForm.add.value = ""; //입력창 clear
 });
 
+//추가 함수
 function addTodoHtml(todo) {
   const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
   <span>${todo}</span>
@@ -25,3 +27,32 @@ list.addEventListener("click", function (e) {
     e.target.parentElement.remove();
   }
 });
+
+//검색창 이벤트
+search.addEventListener("keyup", function (e) {
+  const term = search.search.value.trim(); //검색어
+  removeFilters();
+  if (term) {
+    filterTodos(term);
+  }
+});
+
+//검색어 보여주기
+search.addEventListener("submit", function (e) {
+  e.preventDefault(); //엔터해도 전송x
+});
+
+//검색어로 할일들을 필터링
+function filterTodos(term) {
+  //할일 태그들을 배열로 만듬
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+}
+
+//검색어가 없을때 모두 출력
+function removeFilters() {
+  Array.from(list.children).forEach((todo) =>
+    todo.classList.remove("filtered")
+  );
+}
